99a38f2cd7cdfb340d590bb9746fc07521ecf723
Ethan Liu <272462809@qq.com>
2025-12-02 22:27:27 +0800

    Fixed for verl

--- datus/tools/verl_tools/verl_db_tool.py
+++ datus/tools/verl_tools/verl_db_tool.py
@@ -1,16 +1,19 @@
 # verl_tools.py - 专门给 Verl 用的工具入口
-from typing import Optional
+import json
+from typing import Optional, Tuple
 
 from datus.configuration.agent_config_loader import load_agent_config
 from datus.tools.db_tools.db_manager import db_manager_instance
 from datus.tools.func_tool import DBFuncTool, FuncToolResult
 from datus.utils.constants import DBType
+from datus.utils.loggings import get_logger
 
 try:
     from verl.tools.base_tool import BaseTool
     from verl.tools.schemas import OpenAIFunctionToolSchema
 except ImportError:
     raise
+logger = get_logger(__name__)
 
 
 class BaseVerlDBTool(BaseTool):
@@ -58,44 +61,65 @@ class BaseVerlDBTool(BaseTool):
 
 
 class DatabaseTool(BaseVerlDBTool):
-    def list_databases(self, catalog: Optional[str] = "", include_sys: Optional[bool] = False) -> FuncToolResult:
+    def execute(
+        self, instance_id: str, catalog: Optional[str] = "", include_sys: Optional[bool] = False
+    ) -> Tuple[str, float, dict]:
         if self.agent_config.db_type == DBType.SQLITE:
-            return FuncToolResult(result=[])
-        return self._tool_adapter("").list_databases(catalog, include_sys)
+            result = FuncToolResult(result=[])
+        else:
+            result = self._tool_adapter("").list_databases(catalog, include_sys)
+        logger.debug(f"List database for {instance_id} result: {result}")
+        return json.dumps(result, ensure_ascii=False), 0.0, {}
 
 
 class SchemaTool(BaseVerlDBTool):
-    def list_schemas(
-        self, catalog: Optional[str] = "", database: Optional[str] = "", include_sys: bool = False
-    ) -> FuncToolResult:
-        return self._tool_adapter(database).list_schemas(catalog, database, include_sys)
+    def execute(
+        self, instance_id: str, catalog: Optional[str] = "", database: Optional[str] = "", include_sys: bool = False
+    ) -> Tuple[str, float, dict]:
+        result = self._tool_adapter(database).list_schemas(catalog, database, include_sys)
+        if result.success:
+            logger.debug(f"List schemas for {instance_id} result: {len(result.result)}")
+        else:
+            logger.debug(f"List schemas for {instance_id} result: {result}")
+        return json.dumps(result, ensure_ascii=False), 0.0, {}
 
 
 class ListTableTool(BaseVerlDBTool):
-    def list_tables(
+    def execute(
         self,
+        instance_id: str,
         catalog: Optional[str] = "",
         database: Optional[str] = "",
         schema_name: Optional[str] = "",
         include_views: Optional[bool] = True,
-    ) -> FuncToolResult:
-        return self._tool_adapter(database).list_tables(catalog, database, schema_name, include_views)
+    ) -> Tuple[str, float, dict]:
+        result = self._tool_adapter(database).list_tables(catalog, database, schema_name, include_views)
+        if result.success:
+            logger.debug(f"List tables for {instance_id} result: {len(result.result)}")
+        else:
+            logger.debug(f"List tables for {instance_id} result: {result}")
+        return json.dumps(result, ensure_ascii=False), 0.0, {}
 
 
 class DescTableTool(BaseVerlDBTool):
-    def describe_table(
+    def execute(
         self,
+        instance_id: str,
         table_name: str,
         catalog: Optional[str] = "",
         database: Optional[str] = "",
         schema_name: Optional[str] = "",
         table_type: str = "table",
-    ) -> FuncToolResult:
-        return self._tool_adapter(database).describe_table(
+    ) -> Tuple[str, float, dict]:
+        result = self._tool_adapter(database).describe_table(
             table_name, catalog, database, schema_name, table_type=table_type
         )
+        logger.debug(f"Describe tables for {instance_id} result: {result}")
+        return json.dumps(result, ensure_ascii=False), 0.0, {}
 
 
 class QueryTool(BaseVerlDBTool):
-    def read_query(self, sql: str, database: Optional[str] = "") -> FuncToolResult:
-        return self._tool_adapter(database).read_query(sql)
+    def execute(self, instance_id: str, sql: str, database: Optional[str] = "") -> Tuple[str, float, dict]:
+        result = self._tool_adapter(database).read_query(sql)
+        logger.debug(f"Describe tables for {instance_id} result: {result}")
+        return json.dumps(result, ensure_ascii=False), 0.0, {}
